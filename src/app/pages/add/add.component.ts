4e82620e9b952a7a73abb2167290befd5bb7fed3
Léo Lessirard <lessirard.leo@gmail.com>
2024-09-24 17:03:51 +0200

    feat: config

--- src/app/pages/add/add.component.ts
+++ src/app/pages/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { HeaderComponent } from '../../shared/components/header/header.component'
 import { TranslateModule } from '@ngx-translate/core'
 import { InputTextModule } from 'primeng/inputtext'
@@ -20,15 +20,11 @@ import { FormsModule } from '@angular/forms'
     templateUrl: './add.component.html',
     styleUrl: './add.component.css',
 })
-export class AddComponent implements OnInit {
+export class AddComponent {
     today: Date = new Date()
-  //TODO: fix maxDate
     date: Date | undefined
     minTemp: number | undefined
     maxTemp: number | undefined
     rain: number | undefined
     wind: number | undefined
-
-    ngOnInit() {
-    }
 }
