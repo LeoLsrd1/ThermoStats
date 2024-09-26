2c017ac85cbc96e0e6bb31c05dd55aae2622332d
Léo Lessirard <lessirard.leo@gmail.com>
2024-09-26 20:30:16 +0200

    style: fixes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -47,18 +47,31 @@ export class DashboardComponent {
       this.chartData = this.dataService.weatherDataToChartData(data, 'day')
     })
     this.dataService.getLastWeekData().subscribe((data) => {
-      this.maxTemp = Math.max(
-        ...data.map((data) => data.maxTemp).filter((temp) => temp !== undefined)
+      this.maxTemp = parseFloat(
+        Math.max(
+          ...data
+            .map((data) => data.maxTemp)
+            .filter((temp) => temp !== undefined)
+        ).toFixed(1)
       )
-      this.minTemp = Math.min(
-        ...data.map((data) => data.minTemp).filter((temp) => temp !== undefined)
+      this.minTemp = parseFloat(
+        Math.min(
+          ...data
+            .map((data) => data.minTemp)
+            .filter((temp) => temp !== undefined)
+        ).toFixed(1)
       )
-      this.rain = data
-        .map((data) => data.rain)
-        .filter((rain) => rain !== undefined)
-        .reduce((acc, rain) => acc + rain, 0)
-      this.wind = Math.max(
-        ...data.map((data) => data.wind).filter((wind) => wind !== undefined)
+      this.rain = parseFloat(
+        data
+          .map((data) => data.rain)
+          .filter((rain) => rain !== undefined)
+          .reduce((acc, rain) => acc + rain, 0)
+          .toFixed(1)
+      )
+      this.wind = parseFloat(
+        Math.max(
+          ...data.map((data) => data.wind).filter((wind) => wind !== undefined)
+        ).toFixed(1)
       )
     })
   }
